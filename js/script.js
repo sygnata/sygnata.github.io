c461cb42e37f36c7a55f8d14efb927813a0ef534
Andre Ferraz <andreluiz_ferraz@hotmail.com>
2021-03-29 17:39:44 -0300

    scroll suave e sobre

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@ ScrollReveal().reveal('.tagline', {
 ScrollReveal().reveal('.punchline', {
 	delay: 2000
 })
+/*SMOKE EFFECT*/
+const text = document.querySelector('.nome');
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>")
+
+const letters = document.querySelectorAll('span');
+for (let i = 0; i < letters.length; i++) {
+	letters[i].addEventListener('mouseover', function () {
+		letters[i].classList.add('active');
+	})
+}
+
+/*Scoll Suave*/
+$('.nav a[href^="#"]').on('click', function (e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+		targetOffset = $(id).offset().top;
+
+	$('html, body').animate({
+		scrollTop: targetOffset - 100
+	}, 500);
+});
