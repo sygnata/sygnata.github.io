ff27a075e6b53cb858e1aeeeca693017ed446f0c
Andre Ferraz <andreluiz_ferraz@hotmail.com>
2021-03-28 22:58:21 -0300

    Menu finalizado

--- js/script.js
+++ js/script.js
@@ -1,21 +1,12 @@
-// NavBar
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-
-  mainNav.classList.toggle('active');
-});
-
 // Scroll Reveal
 ScrollReveal({
-  duration: 1000
+	duration: 1000
 })
 
 ScrollReveal().reveal('.headline')
 ScrollReveal().reveal('.tagline', {
-  delay: 500
+	delay: 500
 })
 ScrollReveal().reveal('.punchline', {
-  delay: 2000
+	delay: 2000
 })
