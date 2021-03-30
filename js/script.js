97f31adcf36e24f0ca969c85e0047d21b49144c5
Andre Ferraz <andreluiz_ferraz@hotmail.com>
2021-03-30 18:11:32 -0300

    Rabbit

--- js/script.js
+++ js/script.js
@@ -101,3 +101,26 @@ function initAnimacaoScroll() {
 	}
 }
 initAnimacaoScroll();
+
+function initAnimacaoScroll1() {
+	const sections = document.querySelectorAll('.js-scroll-icone');
+	if (sections.length) {
+		const windowMetade = window.innerHeight * 0.6;
+
+		function animaScroll() {
+			sections.forEach((section) => {
+				const sectionTop = section.getBoundingClientRect().top;
+				const isSectionVisible = (sectionTop - windowMetade) < 0;
+				if (isSectionVisible)
+					section.classList.add('ativoIcone');
+				else
+					section.classList.remove('ativoIcone');
+			})
+		}
+
+		animaScroll();
+
+		window.addEventListener('scroll', animaScroll);
+	}
+}
+initAnimacaoScroll1();
