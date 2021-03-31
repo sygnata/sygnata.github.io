9dcb5ac4e15298653344a681408f13867012bcea
Andre Ferraz <andreluiz_ferraz@hotmail.com>
2021-03-31 12:05:24 -0300

    Whatsapp button e back to top

--- js/script.js
+++ js/script.js
@@ -124,3 +124,21 @@ function initAnimacaoScroll1() {
 	}
 }
 initAnimacaoScroll1();
+
+function initiBackToTop() {
+	var btn = $('#BtnBackToTop');
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
+	btn.on('click', function (e) {
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: 0
+		}, '300');
+	});
+}
+initiBackToTop();
