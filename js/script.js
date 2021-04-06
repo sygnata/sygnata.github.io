3e14272a900ff0244d1910a0273e5f5dd28c3ff5
Andre Ferraz <andreluiz_ferraz@hotmail.com>
2021-04-06 18:00:55 -0300

    initKonamiCode2 module

--- js/script.js
+++ js/script.js
@@ -13,22 +13,4 @@ initiBackToTop();
 initAnimacaoScroll();
 initAnimacaoScroll1();
 initKonamiCode1();
-
-/*Konami Code*/
-
-
-function initKonamiCode2() {
-	let coelho = document.querySelector('#coelho');
-	coelho.addEventListener('mouseover', function (evento) {
-		console.log(evento)
-
-		// console.log('Você ACERTOU a sequencia!!!')
-		// document.querySelector('#winScreen').dataset.status = 'active'
-		// document.querySelector('#winScreen').innerHTML = "SARAH TE AMO!!!"
-		window.scrollTo(0, 0);
-		document.querySelector('#matrix').classList.toggle('active');
-
-
-	});
-}
 initKonamiCode2();
